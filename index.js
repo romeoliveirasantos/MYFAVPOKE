3312e0ce1ddf4b9eb7f31711c87a81f2c1d68e82
Romário Oliveira <rjob.oliveira@gmail.com>
2022-12-04 14:22:53 -0300

    change diretory assets

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 
 // bulbasaur vars
-const bulbasaur = "url(./../../assets/pokemons/bulbasaur.gif)";
-const ivysaur = "url(./../../assets/pokemons/ivysaur.gif)";
-const venusaur = "url(./../../assets/pokemons/venusaur.gif)"; 
+const bulbasaur = "url(./../assets/bulbasaur.gif)";
+const ivysaur = "url(./../assets/ivysaur.gif)";
+const venusaur = "url(./../assets/venusaur.gif)"; 
 const bulbasaurDesc = "Bulbasaur se assemelha a um pequeno anfíbio/sapo, mas carrega três garras em cada um de seus pés e não tem cauda. Ele também tem olhos grandes e vermelhos e dentes pequenos e afiados. Sua pele é de cor turquesa clara com manchas verdes escuras. Tem três garras nas quatro patas. Sua característica mais notável, é o bulbo em suas costas, que segundo seu registro na Pokédex , foi ali plantado ao nascer."
 const ivysaurDesc = "A aparência de Ivysaur é muito semelhante à de sua forma pré-evoluída, Bulbasaur. Ele ainda mantém a pele turquesa e manchas, junto com seus olhos vermelhos. As presas superiores de Ivysaur agora são maiores e são visíveis fora de sua boca. Além disso, suas orelhas agora estão escurecidas para preto no centro. Carregar o peso do broto nas costas torna as pernas de Ivysaur mais fortes. A mudança mais notável é que o bulbo está começando a florescer, com pétalas rosa visíveis e folhas grandes no fundo da flor."
 const venusaurDesc = "Venusaur é um grande Pokémon semelhante a um anfíbio que é um pouco semelhante em aparência às suas formas pré-evoluídas, Ivysaur e Bulbasaur. Sua pele ainda mantém uma cor turquesa clara, mas não tem mais as manchas e parece mais áspera e escamosa, com saliências aqui e ali. O antigo bulbo em suas costas agora floresceu totalmente em uma flor gigantesca e suas orelhas também se iluminaram. As fêmeas de Venusaur são ligeiramente diferentes porque têm uma semente nas costas, o que mostra que pode dar à luz um novo Bulbasaur."
 
 // charmander vars
-const charmander = "url(./../../assets/pokemons/charmander.gif)";
-const charmeleon = "url(./../../assets/pokemons/charmeleon.gif)";
-const charizard = "url(./../../assets/pokemons/charizard.gif)";
+const charmander = "url(./../assets/charmander.gif)";
+const charmeleon = "url(./../assets/charmeleon.gif)";
+const charizard = "url(./../assets/charizard.gif)";
 const charmanderDesc = "Charmander é um Pokémon reptiliano bípede com corpo principalmente laranja e olhos azuis. Sua parte inferior do peito para baixo e as solas dos pés são de cor creme. Ele tem duas pequenas presas visíveis na mandíbula superior e duas presas menores na mandíbula inferior. Um fogo arde na ponta da cauda fina deste Pokémon e arde lá desde o nascimento de Charmander. A chama pode ser usada como uma indicação da saúde e do humor de Charmander, queimando intensamente quando o Pokémon está forte, fracamente quando está exausto, vacilando quando está feliz e brilhando quando está enfurecido. Diz-se que Charmander morre se sua chama se apagar. No entanto, se o Pokémon estiver saudável, a chama continuará queimando mesmo se ficar um pouco molhada e fumegar na chuva."
 const charmeleonDesc = "Charmeleon é um pequeno Pokémon bípede semelhante a um dinossauro com uma aparência semelhante à de sua forma pré-evoluída, Charmander. Charmeleon difere de Charmander por ter uma cor de pele vermelha muito mais escura, uma estrutura corporal maior, garras e uma saliência semelhante a um chifre na parte de trás da cabeça, semelhante à de um ornitópode. Suas garras são afiadas como navalhas e sua cauda é muito forte."
 const charizardDesc = "Charizard é um grande Pokémon semelhante a um dragão que difere muito de sua forma pré-evoluída. A coloração vermelha da pele de Charmeleon não é mais aparente, já que Charizard parece ter revertido para a coloração laranja/amarela de Charmander. Duas grandes presas podem ser vistas quando sua boca está fechada. Quando sua boca está aberta, suas presas inferiores e uma língua esguia, mas longa, podem ser vistas. O único chifre que estava na parte de trás de sua cabeça agora é dois, um de cada lado. A diferença mais notável entre Charizard e suas formas pré-evoluídas são as grandes asas que se desenvolveram em suas costas, o que dá a Charizard a capacidade de voar."
 
 // squirtle vars
-const squirtle = "url(./../../assets/pokemons/squirtle.gif)";
-const wartortle =  "url(./../../assets/pokemons/wartortle.gif)";
-const blastoise = "url(./../../assets/pokemons/blastoise-mega.gif)";
+const squirtle = "url(./../assets/squirtle.gif)";
+const wartortle =  "url(./../assets/wartortle.gif)";
+const blastoise = "url(./../assets/blastoise-mega.gif)";
 const squirtleDesc = "Squirtle é um pequeno Pokémon azul claro com aparência semelhante a uma tartaruga. Com sua forma aerodinâmica e superfície ranhurada, a concha do Squirtle o ajuda a atravessar a água muito rapidamente. Também oferece proteção em batalha. Como as tartarugas, Squirtle tem uma carapaça que cobre seu corpo com buracos que permitem que seus membros, cauda e cabeça fiquem expostos. Ao contrário de uma tartaruga, Squirtle é normalmente bípede."
 const wartortleDesc = "Wartortle é um pequeno Pokémon bípede semelhante a uma tartaruga com uma aparência semelhante à de sua forma pré-evoluída, Squirtle. Algumas diferenças são que Wartortle desenvolveu garras e dentes mais afiados e maiores, e que suas caudas são maiores e mais fofas do que as de Squirtle. O pelo da cauda de Wartortle escurece com a idade. Wartortle desenvolveu grandes orelhas peludas. Como sua forma pré-evoluída, ele também pode usar sua casca para se proteger. Sua casca carrega os arranhões de muitas batalhas."
 const blastoiseDesc = "Blastoise são grandes Pokémon parecidos com tartarugas com algumas características semelhantes às de suas formas pré-evoluídas. Blastoise tem um grande corpo azul, estômago de cor creme, braços e pernas pequenos e gordos e uma grande concha marrom com bordas brancas, apresentando seus canhões de água. Agora é visivelmente mais volumoso do que suas evoluções anteriores. Como suas formas pré-evoluídas, Blastoise tem uma concha que cobre todo o corpo, para a qual também pode ser retirado. Ao contrário de Squirtle e Wartortle, Blastoise tem grandes canhões de água localizados nos lados superior esquerdo e direito. A água que sai dos canhões é capaz de perfurar o aço grosso."
 
 // pichu vars
-const pichu = "url(./../../assets/pokemons/pichu.gif)";
-const pikachu =  "url(./../../assets/pokemons/pikachu.gif)";
-const raichu = "url(./../../assets/pokemons/raichu.gif)";
+const pichu = "url(./../assets/pichu.gif)";
+const pikachu =  "url(./../assets/pikachu.gif)";
+const raichu = "url(./../assets/raichu.gif)";
 const pichuDesc = "Pichu são Pokémon muito pequenos, gordos e amarelos semelhantes a roedores. Ele tem uma aparência semelhante à sua forma evoluída Pikachu, mas varia de várias maneiras. Pichu tem orelhas em forma de diamante no topo da cabeça, com as bordas pretas e o interior amarelo. Debaixo de sua cabeça há um padrão preto, que lembra um colarinho. Suas caudas são totalmente pretas, e lembram um pequeno raio, semelhante a suas formas evoluídas, que são maiores. Os Pichu também têm bolsas redondas e rosadas nas bochechas, usadas para armazenar eletricidade."
 const pikachuDesc = "Pikachu são Pokémon pequenos e fofos parecidos com ratos. Eles são quase completamente cobertos por pêlo amarelo. Eles têm longas orelhas amarelas com pontas pretas. As costas de um Pikachu têm duas listras marrons e sua grande cauda é notável por ter a forma de um raio, mas sua ponta marrom é quase sempre esquecida. Pikachu tem braços curtos com cinco dedos minúsculos em forehands e três dedos afiados em suas patas traseiras. Em suas bochechas há duas bolsas vermelhas em forma de círculo usadas para armazenar eletricidade. Eles ficam amarelos e faíscam com eletricidade quando estão prestes a usar um ataque elétrico, como o Thunderbolt. Também é conhecido por gerar pequenas ondas de energia elétrica em raiva ou para proteção, como no anime."
 const raichuDesc = "Raichu é um Pokémon semelhante a um roedor, semelhante às suas formas de evolução anteriores. Ao contrário de Pichu ou Pikachu, que têm pelo amarelo, Raichu tem pelo laranja com duas tiras de pelo marrom nas costas. Tem orelhas e pés longos, mas tem braços pequenos e grossos sem dedos perceptíveis. Além disso, seus sacos elétricos são amarelos, ao contrário da coloração vermelha de sua forma anterior. Tem uma barriga branca e roliça, as pontas dos braços e dos pés são marrons. As solas dos pés são bronzeadas e as almofadas das patas na parte inferior são laranja amareladas. Raichu também tem uma cauda preta longa e fina que termina em uma grande forma de raio."
 
 // gastly vars
-const gastly = "url(./../../assets/pokemons/gastly.gif)";
-const haunter =  "url(./../../assets/pokemons/haunter.gif)";
-const gengar = "url(./../../assets/pokemons/gengar.gif)";
+const gastly = "url(./../assets/gastly.gif)";
+const haunter =  "url(./../assets/haunter.gif)";
+const gengar = "url(./../assets/gengar.gif)";
 const gastlyDesc = "A aparência de Gastly consiste em uma esfera escura sua 'cabeça' é cercada por gás roxo claro. Gastly tem grandes olhos brancos e uma boca grande com um pequeno par de presas.Eles capturam as presas envolvendo-as em seu gás venenoso, que pode penetrar na pele e causar desmaios. Eles também podem imobilizar a presa com o movimento paralisante Lick. Apesar de sua má reputação, os Gastly não são abertamente maliciosos e não “assombram” os seres com a intenção de prejudicá-los. Em vez disso, eles são brincalhões travessos que gostam de surpreender os outros. Eles raramente utilizam seus gases venenosos, a menos que sejam ameaçados, mas são conhecidos por colocar as vítimas para dormir com hipnose. Suas brincadeiras podem ser irritantes às vezes, então deve-se notar que os ventiladores oscilantes são muito eficazes em afastar Gastly porque o Pokémon fica longe de ventos fortes. Quando os Gastly estão próximos, diz-se que o ar terá um cheiro ligeiramente doce."
 const haunterDesc = "Haunter é uma figura roxa, espetada e flutuante com olhos triangulares, pupilas minúsculas, presas e mãos desencarnadas com três dedos e garras.Eles se alimentam da força vital dos seres vivos, esperando na escuridão por presas incautas para assombrar."
 const gengarDesc = "Gengar é um Pokémon de corpo redondo e parecido com uma sombra, com duas orelhas grandes e pontiagudas e pontas curtas alinhadas em suas costas. Gengar tem dois olhos vermelhos e um sorriso sinistro e cheio de dentes. Tem garras nas mãos. Gengar fica em suas duas pernas curtas, mas também pode levitar.Gengar são tipicamente mesquinhos e gostam de atormentar as pessoas, machucando-as. Eles são mais ativos durante as luas cheias, rastejando na calada da noite e se disfarçando de sombras."
 
 // abra vars
-const abra = "url(./../../assets/pokemons/abra.gif)";
-const kadabra =  "url(./../../assets/pokemons/kadabra.gif)";
-const alakazam = "url(./../../assets/pokemons/alakazam.gif)";
+const abra = "url(./../assets/abra.gif)";
+const kadabra =  "url(./../assets/kadabra.gif)";
+const alakazam = "url(./../assets/alakazam.gif)";
 const abraDesc = "Abra é um Pokémon raposa marrom dourado com três dedos nas mãos e nas pernas. Seus olhos estão quase sempre fechados por causa de sua tendência a dormir muito. Os ombros são castanhos. Abra tem uma longa cauda com uma faixa marrom. Os pés têm 3 garras afiadas, 2 na frente e 1 no calcanhar. Abra mede 2'11 'e tem a mesma aparência física, não importa o gênero. Tem orelhas ligeiramente pontudas e seu corpo parece um pouco com uma espécie de armadura corporal na metade superior."
 const kadabraDesc = "Kadabra tem uma anatomia semelhante a Abra, no entanto, existem algumas diferenças. Kadabra segura uma colher de metal forjada por seus poderes psíquicos. Além disso, a cauda de Kadabra é grande e encaracolada. Ele também tem um 'bigode'. O bigode é maior no Kadabra masculino. O estômago de Kadabra tem três listras vermelhas, enquanto sua testa tem uma estrela vermelha de cinco pontas.Kadabra pode ter a habilidade Synchronize ou a habilidade Inner Focus . Não possui um corpo físico forte, mas prefere usar seus poderes psíquicos."
 const alakazamDesc = "Alakazam é ​​um Pokémon humanoide. É colorido de amarelo e marrom e tem algumas características de raposa. Também é mostrado carregando duas colheres, que usa para lutar com fortes poderes telecinéticos. Como Kadabra, o gênero de Alakazam pode ser identificado pelo comprimento de seu bigode."
